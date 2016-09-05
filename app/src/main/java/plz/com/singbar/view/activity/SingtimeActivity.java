4adc2dc8163888a43eada2ef56a5075cd0abcaec
wq <1061516765@qq.com>
2016-09-05 09:54:11 +0800

    22222

--- app/src/main/java/plz/com/singbar/view/activity/SingtimeActivity.java
+++ app/src/main/java/plz/com/singbar/view/activity/SingtimeActivity.java
@@ -12,6 +12,6 @@ public class SingtimeActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_singtime);
+
     }
 }
--- app/src/main/java/plz/com/singbar/view/frag/Nationwidebar.java
+++ app/src/main/java/plz/com/singbar/view/frag/Nationwidebar.java
@@ -17,7 +17,7 @@ public class Nationwidebar extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_nationwidebar, container, false);
+     //   View view = inflater.inflate(R.layout.activity_nationwidebar, container, false);
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
